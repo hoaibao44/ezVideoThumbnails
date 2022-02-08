4a663ba1a1f3de7e772b59ccac8ef9600b5b06b6
hoaibao44 <hoaibao.nhb@gmail.com>
2022-02-08 19:02:55 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,99 @@
+from mainHandyFunc import *
+import cv2
+
+
+def video2thumb_by_time(file_path, time, output_file):
+    # read video
+    vcap = cv2.VideoCapture(file_path)
+
+    fps_int = round(vcap.get(cv2.CAP_PROP_FPS))  # get fps
+    total_length = vcap.get(cv2.CAP_PROP_FRAME_COUNT) / \
+        vcap.get(cv2.CAP_PROP_FPS)
+
+    # print('video solution {}x{}'.format(
+    #     vcap.get(cv2.CAP_PROP_FRAME_WIDTH), vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    # if time surpass total length then time = 1 (s)
+    if time > total_length:
+        time = 1
+
+    iFrame = 0
+    while True:
+        iFrame += 1
+        ret, frame = vcap.read()
+
+        if not ret:
+            return False
+
+        if round(iFrame/fps_int) > time:
+            cv2.imwrite(output_file, frame)
+            return True
+
+
+def auto_generated():
+
+    all_files = get_cr_folder(input_path)
+
+    output_pd = pd.DataFrame()
+    for cl in ['in_file_name', 'in_file_path', 'out_file_1s', 'out_file_2s', 'out_file_3s']:
+        output_pd[cl] = ''
+
+    for idx, my_file in enumerate(all_files):
+        file_path = all_files[idx][1]
+
+        output_pd.loc[idx] = [all_files[idx][0], all_files[idx][1], '', '', '']
+
+        times = [1, 2, 3]
+        for time in times:
+            output_file = os.path.join(
+                output_path, '{}-sec_{}.jpg'.format(all_files[idx][0], str(time)))
+
+            if video2thumb_by_time(file_path, time, output_file):
+                output_pd['out_file_{}s'.format(str(time))][idx] = output_file
+
+    output_pd.to_excel('outputFile.xlsx', index=False)
+    fommatExcelFile(os.path.join(base_path, 'outputFile.xlsx'))
+
+
+if __name__ == "__main__":
+    global base_path, input_path, output_path
+
+    base_path = getWhereIsMain()
+    input_path = os.path.join(base_path, '1_input')
+    output_path = os.path.join(base_path, '2_output')
+    # not display warnings
+    warnings.simplefilter('ignore')
+    logging.basicConfig(format=r'#%(asctime)s:%(levelname)s\\ %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)  # , filename=os.path.join(base_path, '98_log', 'ezTW_runningLog-{}.txt'.format(startTime_str))
+
+    logging.info('Base_path: {base_path}'.format(
+        base_path=base_path))
+
+    output_pd = get_file_info()
+    output_pd = output_pd.fillna('')
+
+    logging.info('Get info OK')
+    output_pd['output_file_path'] = ''
+    output_pd['output_file_name'] = ''
+
+    for idx in range(0, len(output_pd)):
+        file_path = output_pd['file_path'][idx]
+        file_name = output_pd['file_name'][idx].split('.')[0]
+
+        logging.info('Processing -- {}'.format(file_name))
+
+        if output_pd['time'][idx] != 0:
+            time = output_pd['time'][idx]
+        else:
+            time = 2
+        output_file = os.path.join(output_path, '{}.jpg'.format(file_name))
+        if video2thumb_by_time(file_path, time, output_file):
+            output_pd['output_file_path'][idx] = output_file
+            output_pd['output_file_name'][idx] = '{}.jpg'.format(file_name)
+
+    output_pd.to_excel(os.path.join(base_path, '2_output',
+                       'outputFile.xlsx'), index=False)
+    fommatExcelFile(os.path.join(base_path, '2_output', 'outputFile.xlsx'))
+    logging.info('Write output file -- {}'.format(os.path.join(base_path,
+                 '2_output', 'outputFile.xlsx')))
+    os.system("pause")
---
+++ mainHandyFunc.py
@@ -0,0 +1,175 @@
+# for Logging
+import logging
+import warnings
+
+import json
+import pandas as pd
+import openpyxl
+
+# for path processing
+import os
+
+import openpyxl.styles as styl
+
+
+def getWhereIsMain():
+    if os.path.exists(os.path.join(os.getcwd(), 'pythumb_base_path.txt')):
+        with open(os.path.join(os.getcwd(), 'pythumb_base_path.txt'), 'r', encoding='utf-16') as file:
+            base_path = file.read().replace('\n', '')
+    else:
+        base_path = os.getcwd()
+    return base_path
+
+
+def get_file_info():
+
+    base_path = getWhereIsMain()
+
+    # get main file name from setting json
+    with open(os.path.join(base_path, '0_setting', 'setting.json'), encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    logging.info('Main File -- ' + os.path.join(base_path, data['main_file']))
+
+    wb = openpyxl.load_workbook(os.path.join(base_path, data['main_file']))
+    sheet_go = wb['GO']
+    data_dict = {
+        'file_path': [],
+        'file_name': [],
+        'time': [],
+    }
+    for idx in range(7, sheet_go.max_row):
+        if sheet_go['E'+str(idx)].value is not None:
+            data_dict['file_path'].append(sheet_go['E'+str(idx)].value)
+            data_dict['file_name'].append(sheet_go['F'+str(idx)].value)
+            data_dict['time'].append(sheet_go['G'+str(idx)].value)
+
+    return pd.DataFrame.from_dict(data_dict)
+
+
+def changeCl_df(mapping_df, in_df):
+    sub_df = pd.DataFrame()
+    for cl in mapping_df.columns.tolist():
+        if cl == 'FLAG' and 'FLAG' in in_df.columns.tolist():
+            sub_df['FLAG'] = in_df['FLAG']
+        else:
+            if mapping_df[cl][0] != '':
+                if mapping_df[cl][0] in in_df.columns.tolist():
+
+                    sub_df[cl] = in_df[mapping_df[cl][0]]
+
+            else:
+                sub_df[cl] = ''
+
+    sub_df.fillna('')
+
+    return sub_df
+
+
+def readMapping(mediaType='SS'):
+    mainFolder_path = getWhereIsMain()
+
+    # get main file name from setting json
+    with open(os.path.join(mainFolder_path, '0_driver', 'setting.json'), encoding='utf-8') as json_file:
+        setting_json = json.load(json_file)
+
+    filePath = os.path.join(
+        mainFolder_path, '0_driver', setting_json['columnMapping'])
+
+    mapping_df = pd.read_excel(
+        filePath, sheet_name='adsInfoCLMapping', header=0, engine='openpyxl')
+    mapping_df = mapping_df.fillna('')
+    mapping_df = mapping_df[mapping_df['FLAG']
+                            == mediaType].reset_index(drop=True)
+
+    mapping_df.fillna('')
+
+    return mapping_df
+
+
+def fommatExcelFile(filePath):
+    # def style
+    allCell_style = styl.NamedStyle(name="allCell_style")
+    allCell_style.font = styl.Font(name="メイリオ", size=8)
+    allCell_style.alignment = styl.Alignment(
+        horizontal="left", vertical="center")
+
+    # open file
+    wb1 = openpyxl.load_workbook(filePath)
+    for ws in wb1.worksheets:
+        for row in ws:
+            for cell in row:
+                cell.style = allCell_style  # apply style to each cell
+
+    # save wb
+    wb1.save(filePath)
+    wb1.close()
+
+    """
+    # %%
+    theURL = 'https://ads.twitter.com/data/composer/18ce53vzsrq/api/tweet?as_user_id=2517371725&text=python-test-1216-1539&card_uri=card%3A%2F%2F1468903518165954560&nullcast=true'
+    needed_cookie_array = ['_ga', '_gid', '_twitter_sess', 'att', 'auth_token', 'ct0', 'gt',
+                            'guest_id', 'guest_id_ads', 'guest_id_marketing', 'kdt', 'lang', 'personalization_id', 'twid']
+    the_cookies = {}
+    for ck in needed_cookie_array:
+        the_cookies[ck] = firefox.get_cookie(ck)['value']
+
+    response = requests.post(theURL, headers=the_headers, cookies=the_cookies)
+    print(response.status_code)
+
+    # %%
+    import itertools
+    ck_comp_list = []
+    needed_cookie_array = ['_ga', '_gid', '_twitter_sess', 'att', 'auth_token', 'ct0', 'gt',
+                            'guest_id', 'guest_id_ads', 'guest_id_marketing', 'personalization_id']
+
+    for i in range(3, len(needed_cookie_array)):
+        comp_ojb = itertools.combinations(needed_cookie_array, i)
+        ck_comp_list += list(comp_ojb)
+
+    final_comp = []
+    for ck_comp in ck_comp_list:
+        the_cookies = {}
+        for ck in ck_comp:
+            the_cookies[ck] = firefox.get_cookie(ck)['value']
+
+        response = requests.post(
+            theURL, headers=the_headers, cookies=the_cookies)
+        if response.status_code != 403:
+            final_comp.append(ck_comp)
+            break
+        else:
+            print('{} - {}'.format(response.status_code, ck_comp))
+        sleep(3)
+
+    the_data = {
+        'as_user_id': '2517371725',
+        'text': 'python-test_2021/12/16-maruchi',
+        'scheduled_at': '2021-12-17T10:57:17Z',
+        'media_ids': '1469652676216782851',
+        'nullcast': 'nullcast'
+    }
+
+    script_text = '''var xmlHttp = new XMLHttpRequest();
+    theUrl = 'https://ads.twitter.com/data/composer/18ce53vzsrq/api/tweet?as_user_id=2517371725&text=python-test-1217-1654&card_uri=card%3A%2F%2F1468903518165954560&nullcast=true';
+    thepayLoad ='{}'
+    xmlHttp.open("POST", theUrl, false);
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.send(thepayLoad);
+    return xmlHttp.responseText'''
+
+    result = firefox.execute_script(script_text)
+    result_json = json.loads(result)
+    """
+
+
+def get_cr_folder(cr_path):
+    # get all file in CR_folder
+    # return as an array
+    logging.info('FUNC(login): get_cr_folder')
+    out_array = []
+    for path, subdirs, files in os.walk(cr_path):
+        for name in files:
+            # logging.info(os.path.join(path,name))
+            out_array.append([name, os.path.join(path, name)])
+    return out_array
